c4080da0df9c61a1db6fdfeffe2543e26b22ee0c
Richieon88 <>
2023-10-13 17:44:53 +0000

    Refine build board and print board functions

--- run.py
+++ run.py
@@ -1,35 +1,11 @@
 from random import randint
 
-
-board = []
-for x in range(0, 6):
-    board.append(["-"] * 6)
+def build_board(dims):
+    return [['-' for count in range(dims)] for count in range(dims)]
 
 def print_board(board):
-  for row in board:
-    print(" ".join(row))
+    for b in board:
+        print(*b)
 
-def random_row(board):
-  return randint(0, len(board) - 1)
-
-def random_column(board):
-  return randint(0, len(board[0]) - 1)
-
-
-ship_row = random_row(board)
-ship_column = random_column(board)
-
-print_board(board)
-print(ship_row)
-print(ship_column)
-
-input_row = int(input("Guess ship row:"))
-input_column = int(input("Guess ship column:"))
-
-
-if input_row == ship_row and input_column == ship_column:
-    print("You sunk my battleship")
-else:
-    print("you missed")
-    board[input_row][input_column] = "X"
-    print_board(board)    
+board = build_board(6)
+print_board(board)           
