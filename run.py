b24117e7cc5991cb8175ccda26c8a79a9aa7a460
Richieon88 <>
2023-10-13 19:34:04 +0000

    Fix bug with place ship function

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from random import randint
+import random
 
 board = [['-' for _ in range(5)] for _ in range(5)]
 
@@ -7,8 +7,15 @@ def print_board(board):
     for row in board:
         print(" ".join(row))
 
-def place_battleship(board):
+def place_ship(board):
     row = random.randint(0, 4)
     col = random.randint(0, 4)
     return (row, col)
 
+
+print("Let's play Battleship!")
+print_board(board)
+
+ships = [(place_ship(board), place_ship(board), place_ship(board))]
+guess_row = int(input("Guess Row: "))
+guess_col = int(input("Guess Column: "))
