7dfe78d5ce1786d1f7bc4bc0706a46be1fbcfc81
Richieon88 <>
2023-10-14 23:59:03 +0000

    Add function for computer to hit player ships

--- run.py
+++ run.py
@@ -11,8 +11,8 @@ def print_board(board):
 def place_ship(board):
     while True:
         try:
-            row = int(input("Enter the row to place your ship (0-4): "))
-            col = int(input("Enter the column to place your ship (0-4): "))
+            row = int(input("Enter the row to place your ship (0-4): \n"))
+            col = int(input("Enter the column to place your ship (0-4): \n"))
             if 0 <= row <= 4 and 0 <= col <= 4 and board[row][col] == '-':
                 return (row, col)
             if 0 <= row <= 4 and 0 <= col <= 4 and board[row][col] == '-':
@@ -38,7 +38,7 @@ computer_ships = set()
 
 for turn in range(8):
     print(f"Turn {turn + 1}")
-
+    #Players Turn
     print("Your turn")
     guess_row = int(input("Guess Row: \n"))
     guess_col = int(input("Guess Col: \n"))
@@ -58,3 +58,14 @@ for turn in range(8):
     if not computer_ships:
         print("You win! You've destroyed all of the computer's ships.")
         break
+
+    #Computer Turn
+    Print("Computers Turn")
+    guess_row = random.randint(0, 4)
+    guess_col = random.randint(0, 4)
+
+    if (guess_row, guess_col) in player_ships:
+        print("Computer hit one of your battleships!")
+        board_player[guess_row][guess_col] = "H"
+        player_ships.remove((guess_row, guess_col))
+        print_board(board_player)
