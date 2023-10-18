2a1e404af81b6ad4a2bc57583424a9ed8e64b25e
Richieon88 <>
2023-10-18 20:53:02 +0000

    Refactor game loop in into functions

--- run.py
+++ run.py
@@ -2,13 +2,8 @@ import os
 import random
 from time import sleep
 
-
 def clear():
-    """
-    This clears the terminal to prevent clutter on it.
-    """
-    os.system('cls' if os.name=='nt' else 'clear')
-# Initialize player and computer boards
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def print_board(board):
     for row in board:
@@ -25,8 +20,51 @@ def place_ship(board, ship_symbol):
                 print("Invalid placement. Try again.")
         except ValueError:
             print("Invalid input. Enter numbers between 0 and 4.")
-            
-            
+
+def display_boards(player_board, computer_board, message):
+    player_title = "Player's Board"
+    computer_title = "Computer's Board"
+    clear()
+    print(f"{player_title:15}{' ' * 5}{computer_title}")
+    for player_row, computer_row in zip(player_board, computer_board):
+        player_row_str = ' '.join(player_row)
+        computer_row_str = ' '.join(computer_row)
+        print(f"{player_row_str:15}{' ' * 5}{computer_row_str}")
+    print(message)
+
+def player_turn(board_player, board_computer, computer_ships):
+    display_boards(board_player, board_computer, "Your turn")
+    player_guess_row, player_guess_col = validate_guess([])
+
+    target = board_computer[player_guess_row][player_guess_col]
+    print(target)
+    if target in computer_ships:
+        board_computer[player_guess_row][player_guess_col] = "H"
+        computer_ships.remove(target)
+        display_boards(board_player, board_computer, "You hit a battleship!")
+    else:
+        display_boards(board_player, board_computer, "You missed!")
+        board_computer[player_guess_row][player_guess_col] = "X"
+
+def computer_turn(board_player, board_computer, player_ships):
+    display_boards(board_player, board_computer, "Computer's Turn")
+    sleep(2)
+    while True:
+        computer_guess_row = random.randint(0, 4)
+        computer_guess_col = random.randint(0, 4)
+        if board_player[computer_guess_row][computer_guess_col] not in {"H", "X"}:
+            break
+
+    target = board_player[computer_guess_row][computer_guess_col]
+    if target in player_ships:
+        board_player[computer_guess_row][computer_guess_col] = "H"
+        player_ships.remove(target)
+        display_boards(board_player, board_computer, "Computer hit your battleship!")
+        sleep(2)
+    else:
+        display_boards(board_player, board_computer, "Computer missed!")
+        sleep(2)
+
 def validate_guess(guesses_made):
     LINE_FLUSH = '\r\033[K'
     UP_FRONT_LINE = '\033[F'
@@ -36,11 +74,9 @@ def validate_guess(guesses_made):
             try:
                 row = int(row)
             except ValueError:
-                print(UP_FRONT_LINE + LINE_FLUSH + "Invalid input! Choose 0-4!")                
-                # print("Invalid input! Choose 0-4!\n")
+                print(UP_FRONT_LINE + LINE_FLUSH + "Invalid input! Choose 0-4!")
                 continue
-            if row not in range(4):
-                # print("Invalid input! Choose 0-4!\n")
+            if row not in range(5):  # Change this range from 4 to 5 to allow entering '4'.
                 print(UP_FRONT_LINE + LINE_FLUSH + "Invalid input! Choose 0-4!")
                 continue
             break
@@ -49,35 +85,17 @@ def validate_guess(guesses_made):
             try:
                 col = int(col)
             except ValueError:
-                # print("Invalid input! Choose 0-4!\n")
                 print(UP_FRONT_LINE + LINE_FLUSH + "Invalid input! Choose 0-4!")
                 continue
-            if col not in range(5):
-                # print("Invalid input! Choose 0-4!\n")
+            if col not in range(5):  # Change this range from 4 to 5 to allow entering '4'.
                 print(UP_FRONT_LINE + LINE_FLUSH + "Invalid input! Choose 0-4!")
                 continue
             break
         if (row, col) in guesses_made:
-            # print("You have already made this move")
             print(UP_FRONT_LINE + LINE_FLUSH + "You have already made this move")
             continue
         break
     return row, col
-
-def display_boards(player_board, computer_board, message):
-    player_title = "Player's Board"
-    computer_title = "Computer's Board"
-    clear()
-    print(f"{player_title:15}{' ' * 5}{computer_title}")
-
-    for player_row, computer_row in zip(player_board, computer_board):
-        # Join the elements in each row of both boards with a space
-        player_row_str = ' '.join(player_row)
-        computer_row_str = ' '.join(computer_row)
-        
-        # Print the player's and computer's rows under their respective titles
-        print(f"{player_row_str:15}{' ' * 5}{computer_row_str}")
-    print(message)
 
 def main():
     board_player = [['-' for _ in range(5)] for _ in range(5)]
@@ -88,15 +106,14 @@ def main():
     playing = True
     
     while playing:
-
         turns = 8
         guesses = []
-    # Place player's ships
+
+        # Place player's ships
         print("Place your ships.")
         for ship_symbol in player_ships:
             ship_row, ship_col = place_ship(board_player, ship_symbol)
             board_player[ship_row][ship_col] = ship_symbol
-            # print_board(board_player)
             display_boards(board_player, board_computer, "Place your ships.")
 
         # Place computer's ships
@@ -109,73 +126,29 @@ def main():
                     board_computer[ship_row][ship_col] = ship_symbol
                     break
 
-
         for turn in range(turns):
             print(f"Turn {turn + 1}")
-            # Player's Turn
-            # print_board(board_computer)  # Display the computer board
-            display_boards(board_player, board_computer, "Your turn")
-            
-            # print("Your turn")
-            # player_guess_row = int(input("Guess Row: "))
-            # player_guess_col = int(input("Guess Col: "))
-            player_guess_row, player_guess_col = validate_guess(guesses)
-            guesses.append((player_guess_row, player_guess_col))
 
-            target = board_computer[player_guess_row][player_guess_col]
-            print(target)
-            if target in computer_ships:
-                # print(f"You hit a battleship!")
-                board_computer[player_guess_row][player_guess_col] = "H"
-                computer_ships.remove(target)
-                # print_board(board_computer)
-                display_boards(board_player, board_computer, "You hit a battleship!")
-                
-            else:
-                # print("You missed!")
-                display_boards(board_player, board_computer, "You missed!")
-                # print_board(board_computer)
-                board_computer[player_guess_row][player_guess_col] = "X"
+            # Player's Turn
+            player_turn(board_player, board_computer, computer_ships)
 
             if not computer_ships:
                 print("You win! You've destroyed all of the computer's ships.")
                 playing = False
-                break # Player wins, exit the loop
+                break
 
             # Computer Turn
-            
-            # print_board(board_player)  # Display the player board
-            display_boards(board_player, board_computer, "Computer's Turn")
-            # print("Computer's Turn")
-            sleep(2)
-            while True:
-                computer_guess_row = random.randint(0, 4)
-                computer_guess_col = random.randint(0, 4)
-                if board_player[computer_guess_row][computer_guess_col] not in {"H", "X"}:
-                    break
-
-            target = board_player[computer_guess_row][computer_guess_col]
-            if target in player_ships:
-                # print(f"Computer hit your battleship!")
-                board_player[computer_guess_row][computer_guess_col] = "H"
-                player_ships.remove(target)
-                display_boards(board_player, board_computer, "Computer hit your battleship!")
-                sleep(2)
-                # print_board(board_player)
-            else:
-                print("Computer missed!")
-                board_player[computer_guess_row][computer_guess_col] = "X"
-                display_boards(board_player, board_computer, "Computer missed!")
-                sleep(2)
-                # print_board(board_player)
+            computer_turn(board_player, board_computer, player_ships)
 
             if not player_ships:
                 print("Computer wins! It has destroyed all of your ships.")
-                playing = False  # Computer wins, exit the loop
+                playing = False
+
     play_again = input("Do you want to play again? Y or N")
     if play_again.lower().startswith("y"):
         main()
     else:
         print("Thanks for playing!")
 
-main()
+if __name__ == "__main__":
+    main()
